8306140e947b431ff1544d159cf711ac7904448b
Andreyome <andreyomem@gmail.com>
2022-12-06 21:27:06 +0200

    fix 1

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+int aliquot = 0;
+        for (int i = 1; i < first; i++)
+            if (first % i == 0) {
+                aliquot += i;
+            }
+        if (second == aliquot) {
+            System.out.println("Aliquot");
+        } else {
+            System.out.println("Not aliquot");
+        }
     }
 }
