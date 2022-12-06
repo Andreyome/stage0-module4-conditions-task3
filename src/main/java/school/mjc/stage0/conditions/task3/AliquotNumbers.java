97dcdcc3d5b5835116308fd727c278c112c9d8db
Andreyome <andreyomem@gmail.com>
2022-12-06 21:44:08 +0200

    aliquot task not quite understandable

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,15 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
-int aliquot = 0;
-        for (int i = 1; i < second; i++)
-            if (second % i == 0) {
-                aliquot += i;
-            }
-        if (first == aliquot) {
-            System.out.println("Aliquot");
-        } else {
-            System.out.println("Not aliquot");
-        }
+if(second % first ==0){
+System.out,.println("Aliquot");
+}
+else {
+System.out.println("Not aliquot");
+}
     }
 }
